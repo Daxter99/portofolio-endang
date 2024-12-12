7aa8cafc9071137c767d05d97b86fd65b0121e42
Daxter99 <jajakjuni7@gmail.com>
2024-12-12 23:41:19 +0700

    my changes

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ window.onbeforeunload = () => {
     form.reset();
   }
 };
+
+
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
